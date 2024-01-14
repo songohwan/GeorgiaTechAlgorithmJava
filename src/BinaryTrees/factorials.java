c7e9ea129118f0b65dfbd893bf32475c11fdb1c1
songohwan <songohwan@knou.ac.kr>
2024-01-14 16:37:54 +0900

    factorials. supplment materials to understand recursive calls

---
+++ src/BinaryTrees/factorials.java
@@ -0,0 +1,19 @@
+package BinaryTrees;
+
+
+
+public class factorials {
+    public static void main(String[] args) {
+        int n = 10; // Example, you can change this value or take input from user
+        System.out.println("Factorial of " + n + " is: " + factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n + factorial(n - 1);
+        }
+    }
+
+}
